0e2828a5e52fcc9d2fd4710f59c20735f43eb8c0
hansonivander <hansonivander@gmail.com>
2024-01-19 23:55:18 +0700

    WearHouse

---
+++ WearHouse/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WearHouse.Models;
+
+namespace WearHouse.Data
+{
+    public class ApplicationDbContext: IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) {}
+        public DbSet<Categories> WearHouse {  get; set; }
+
+    }
+}
---
+++ WearHouse/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WearHouse.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string Category { get; set; }
+    }
+}
